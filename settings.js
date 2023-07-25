3fa6c9f4d3076771ea05401a7d919465c1df3682
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-07-25 12:09:49 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://cluster0.oyfwvmp.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "anyyaapii@gmail.com" //email
+global.email_password = "aniiaapii5546$" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
